7e4c5a58e62da60c0a54ec66da8538b54c8ef423
yu <hansiyu@evereasycom.cn>
2023-08-29 10:47:52 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -33,10 +33,7 @@ const  collapseTitle = () => import( '../pages/collapseTitle.vue' )
 const  map = () => import( '../pages/map.vue' )
 const  pixiJs = () => import( '../pages/pixiJs.vue' )
 const  dealPhoneNum = () => import('../pages/dealPhoneNum.vue')
-<<<<<<< HEAD
 const  gradualChangeProgressBar = () => import('../pages/gradualChangeProgressBar.vue')
-=======
->>>>>>> 814b161fb727151ecdee19f4e02cfdc9b0bfb503
 const  tableCompare = () => import('../pages/tableCompare.vue')
 const  many_tag = () => import('../pages/many_tag.vue')
 
@@ -82,10 +79,7 @@ const router = new VueRouter({
                 { path: 'tcPlayer', component: tcPlayer, meta:{name:'videoJs 倍速播放'}},
                 { path: 'map', component: map, meta:{name:'map'}},
                 { path: 'dealPhoneNum', component: dealPhoneNum, meta:{name:'手机号中间4位脱敏'}},
-<<<<<<< HEAD
                 { path: 'gradualChangeProgressBar', component: gradualChangeProgressBar, meta:{name:'音乐播放'}},
-=======
->>>>>>> 814b161fb727151ecdee19f4e02cfdc9b0bfb503
                 { path: 'tableCompare', component: tableCompare, meta:{name:'单击右侧表格往左边插入'}},
                 { path: 'many_tag', component: many_tag, meta:{name:'循环多个tag并点击跳转+ 下拉框多选 '}},
 
