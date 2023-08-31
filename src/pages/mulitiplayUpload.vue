21d4b89eaa95868f2a05e095f26bc0de428d76f6
Long8214 <15536685239@163.com>
2023-08-31 13:40:28 +0800

    更换文件上传action

--- src/pages/mulitiplayUpload.vue
+++ src/pages/mulitiplayUpload.vue
@@ -1,41 +1,34 @@
 <template>
-<div>
-  <el-upload 
-              ref="upload" 
-              :limit="10" 
-              accept=".jpg,.gif,.png,.jpeg,.txt,.pdf,.doc,.docx,.xls,.xlsx" 
-              name="files" 
-              :multiple="true"
-              :action="baseURL"
-              :headers="myToken" 
-              :on-change="handleFileChange"
-              :before-remove="handleFileRemove" 
-              :auto-upload="false"
-              :file-list="upload.fileList" 
-              >
-              <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-            </el-upload>
-            <el-button type="primary" @click="submitFileForm">确 定</el-button>
-
-
-
-
-</div>
-
+  <div>
+    <el-upload
+      ref="upload"
+      :limit="10"
+      accept=".jpg,.gif,.png,.jpeg,.txt,.pdf,.doc,.docx,.xls,.xlsx"
+      name="files"
+      :multiple="true"
+      action="https://jsonplaceholder.typicode.com/posts/"
+      :on-change="handleFileChange"
+      :before-remove="handleFileRemove"
+      :auto-upload="false"
+      :file-list="upload.fileList"
+    >
+      <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+    </el-upload>
+    <el-button type="primary" @click="submitFileForm">确 定</el-button>
+  </div>
 </template>
 <script>
-    export default{
-      data(){
-	return {
+export default {
+  data() {
+    return {
       upload: {
         fileList: [],
-        fileName: []
+        fileName: [],
       },
-	}
-},
+    };
+  },
 
-
-        methods: {
+  methods: {
     // 上传发生变化钩子
     handleFileChange(file, fileList) {
       this.upload.fileList = fileList;
@@ -49,32 +42,29 @@
       // 创建新的数据对象
       let formData = new FormData();
       // 将上传的文件放到数据对象中
-      this.upload.fileList.forEach(file => {
-        formData.append('file', file.raw);
+      this.upload.fileList.forEach((file) => {
+        formData.append("file", file.raw);
         this.upload.fileName.push(file.name);
       });
-      console.log("提交前",formData.getAll('file'));
-
+      console.log("提交前", formData.getAll("file"));
       // 文件名
-      formData.append('fileName', this.upload.fileName);
+      formData.append("fileName", this.upload.fileName);
       // 自定义上传
-      this.$api.uploadFile(formData).then(response => {
-          console.log(response);
-          // if(response.code == 200){
-          //   this.$refs.upload.clearFiles();
-          //   this.msgSuccess('上传成功！');
-          // }else{
-          //   this.$message.error(response.msg);
-          // }
-        })
-        .catch(error => {
-          this.$message.error('上传失败！');
-        });
-
+      // this.$api
+      //   .uploadFile(formData)
+      //   .then((response) => {
+      //     console.log(response);
+      //     // if(response.code == 200){
+      //     //   this.$refs.upload.clearFiles();
+      //     //   this.msgSuccess('上传成功！');
+      //     // }else{
+      //     //   this.$message.error(response.msg);
+      //     // }
+      //   })
+      //   .catch((error) => {
+      //     this.$message.error("上传失败！");
+      //   });
     },
-
-
-
-        }
-    }
+  },
+};
 </script>
